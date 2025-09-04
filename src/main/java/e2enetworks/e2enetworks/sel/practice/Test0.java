58516feb03f686bfa16a5323ca1374befdf14f4b
rachit1810 <rachit1810@gmail.com>
2025-09-04 19:02:29 +0530

    Second commit

---
+++ src/main/java/e2enetworks/e2enetworks/sel/practice/Test0.java
@@ -0,0 +1,11 @@
+package e2enetworks.e2enetworks.sel.practice;
+
+public class Test0 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is the first edit for GitHub");
+
+	}
+
+}
