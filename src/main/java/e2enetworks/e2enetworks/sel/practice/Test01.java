c2528138719c83f09a59edd4084d666676f76e14
rachit1810 <rachit1810@gmail.com>
2025-09-04 19:34:35 +0530

    Commit 2

---
+++ src/main/java/e2enetworks/e2enetworks/sel/practice/Test01.java
@@ -0,0 +1,10 @@
+package e2enetworks.e2enetworks.sel.practice;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the second edit for GitHub");
+
+	}
+
+}
